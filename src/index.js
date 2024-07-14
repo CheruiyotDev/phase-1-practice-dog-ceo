d2be9faf0815b8bf052279fc5a6a2cef972cc316
Cheruiyot Evans <chersoftwaredev@gmail.com>
2024-07-14 18:12:34 +0300

    dog ceo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => {
+      const imageContainer = document.getElementById('dog-image-container');
+
+      // Iterate over each image URL and create <img> elements
+      data.message.forEach(imageUrl => {
+        const img = document.createElement('img');
+        img.src = imageUrl;
+        img.alt = 'Random dog image';
+        imageContainer.appendChild(img);
+      });
+    });
